bac3c70303e44beb502d2ec7648ec721c4763a5a
Jonathan Cole <coleji@bc.edu>
2016-07-16 14:59:28 -0400

    initial version, successfully posts to sendgrid, need exception handling etc

---
+++ relay.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env nodejs
+var express = require('express')
+var bodyParser = require('body-parser')
+var fs = require('fs')
+var ini = require('ini')
+
+var sendgrid = require('./relays/sendgrid')
+
+var app = express();
+var config = ini.parse(fs.readFileSync('./private.ini', 'utf-8'))
+
+// validate legit request
+app.use(function(req, res, next) {
+	console.log("received req")
+	if (req.method != 'POST' || req.path != '/relay') res.end();					// only accept POSTs going to /relay
+	else if (!req.headers['cbi-auth']) res.end();									// cbi-auth header exists?
+	else if (req.headers['cbi-auth'] != config.general.cbiAuthToken) res.end();		// cbi-auth header is correct?
+	else next();
+});
+
+app.use(function(req, res, next) { req.config = config;  next(); })	// attach config
+
+app.use(bodyParser.text());
+
+app.post('/relay', sendgrid);
+
+app.listen(8080, function() {
+	console.log('Example app listening on port 8080!');
+});
---
+++ relays/sendgrid.js
@@ -0,0 +1,45 @@
+var https = require('https');
+
+var relay = function(req, res, next) {
+	req.utf8Body = (new Buffer(req.body,'hex')).toString('utf-8');
+	req.jsonBody = JSON.parse(req.utf8Body)
+
+	req.sgString =	'to=' + encodeURIComponent(req.jsonBody.to) + '&' +
+					'from=' + encodeURIComponent(req.jsonBody.from) + '&' +
+					'subject=' + encodeURIComponent(req.jsonBody.subject) + '&' +
+					'text=' + encodeURIComponent(req.jsonBody.text) + '&' +
+					'html=' + encodeURIComponent(req.jsonBody.html) + '&' +
+					'headers=' + encodeURIComponent(req.jsonBody.headers);
+
+	var options = {
+		hostname: req.config.sendgrid.host,
+		path: req.config.sendgrid.path,
+		method: 'POST',
+		headers : {
+			"Authorization" : req.config.sendgrid.authToken,
+			"Content-Type" : "application/x-www-form-urlencoded; charset=utf-8",
+			"Content-Length" : req.sgString.length,
+			"Content-Encoding" : 'utf-8'
+		}
+	};
+
+	var sgReq = https.request(options, function(sgRes) {
+		console.log('statusCode: ', sgRes.statusCode);
+		console.log('headers: ', sgRes.headers);
+
+		sgRes.on('data', function(d) {
+			console.log("! " + d)
+		})
+
+		res.send('S')
+	});
+
+	sgReq.on('error', function(e) {
+		console.log(e);
+	});
+
+	sgReq.write(req.sgString);
+	sgReq.end();
+};
+
+module.exports = relay;
