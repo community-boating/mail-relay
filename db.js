fbacfe5239166eefec64cba754aa8f1a343e2ad5
root <root@fj.community-boating.org>
2017-04-02 21:42:06 -0400

    close db connections after insert

--- db.js
+++ db.js
@@ -11,6 +11,7 @@ module.exports = function(email) {
 			"Insert into email_log set email_to = ?, email_from = ?, subject = ?, plain_body = ?, html_body = ?, headers = ?, bcc = ?, from_name = ?, sent_date = ?",
 			[email.to, email.from, email.subject, email.text, email.html, email.headers, email.bcc, email.fromName, (new Date())],
 			function(err, results) {
+				connection.end();
 				if (err) reject(err);
 				else resolve();
 			}
